c73d978dbef25928170c8e1da62c37e393548773
EDEN <game.love.meat.0412@icloud.com>
2025-09-22 15:27:15 +0900

    build.gradleにSnakeYAMLとJavaFakerの依存関係を追加し、UserFormFactoryにユーザー名の長さ制限に関するコメントを追加。UserFormUnitTestにバリデーションテストを追加。

--- src/test/java/in/tech_camp/pictweet/form/UserFormUnitTest.java
+++ src/test/java/in/tech_camp/pictweet/form/UserFormUnitTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
 
+import in.tech_camp.pictweet.factory.UserFormFactory;
 import in.tech_camp.pictweet.validation.ValidationPriority1;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
@@ -13,20 +14,29 @@ import jakarta.validation.ValidatorFactory;
 
 @ActiveProfiles("test")
 public class UserFormUnitTest {
+    private UserForm userForm;
+    private Validator validator;
+
+    @BeforeEach
+    public void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+        userForm = UserFormFactory.createUser();
+    }
+
     @Test
     public void nicknameが空の場合バリデーションエラーが発生する() {
-        // UserFormのインスタンスを初期化
-        UserForm userForm = new UserForm();
-        userForm.setNickname(""); // 空のニックネーム
-        userForm.setEmail("test@test.com"); // メールアドレス
-        userForm.setPassword("techcamp123"); // パスワード
-        userForm.setPasswordConfirmation("techcamp123");// 確認用パスワード
-        // バリデーションの実行
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        Validator validator = factory.getValidator();
+        userForm.setNickname("");
         Set<ConstraintViolation<UserForm>> violations = validator.validate(userForm, ValidationPriority1.class);
-        // アサーション
         assertEquals(1, violations.size());
         assertEquals("Nickname can't be blank", violations.iterator().next().getMessage());
+    }
+
+    @Test
+    public void emailが空の場合バリデーションエラーが発生する() {
+        userForm.setEmail("");
+        Set<ConstraintViolation<UserForm>> violations = validator.validate(userForm, ValidationPriority1.class);
+        assertEquals(1, violations.size());
+        assertEquals("Email can't be blank", violations.iterator().next().getMessage());
     }
 }
